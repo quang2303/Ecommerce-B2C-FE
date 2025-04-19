9f3bfa275fa903598a599761a54e427281be69d7
quang2303 <chungducquang2@gmail.com>
2025-04-19 12:09:43 +0700

    update

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosClient = axios.create({
-  baseURL: "http://127.0.0.1:3000",
+  baseURL: "https://ptadmin-seven.vercel.app/",
   headers: {
     "Content-Type": "application/json",
   },
